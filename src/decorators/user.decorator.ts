fdd2254402a2aaa1c0765390108d7aed9e2bb4af
Gall Atto <attogall@icloud.com>
2023-03-13 21:22:27 +0200

    fix(userDocument): change import spalling

--- src/decorators/user.decorator.ts
+++ src/decorators/user.decorator.ts
@@ -1,5 +1,5 @@
-import { UserDocukment } from 'src/users/schemas/user.schema';
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+import { UserDocument } from 'src/users/schemas/user.schema';
 
 export const CurrentUser = createParamDecorator(
   (_data: unknown, ctx: ExecutionContext) => {
